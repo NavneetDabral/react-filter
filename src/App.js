3e51b2ea99b3fa84e3e628453c1b350bfa26a702
Navneet Dabral <dabralnavneet@gmail.com>
2019-09-23 15:55:37 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import DataSource from './app/datasource';
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+         <h2>POSTS</h2>
+        <DataSource></DataSource>
+    </div>
+  );
+}
+
+export default App;
